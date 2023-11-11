00fcb511cbe8963b68b3d0b24f16c0dbeb952ee6
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2023-11-11 21:25:31 +0700

    mengimplementasikan atomic design dan menambahkan isi Input

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react"
 import Button from "./components/Elements/Button"
+import InputForm from "./components/Elements/input/Index"
 
 const App = () => {
   return (
-    <div className="flex justify-center bg-blue-700 min-h-screen items-center">
-      <div className="flex gap-x-3">
-        <Button variant="bg-red-700">Login</Button>
-        <Button variant="bg-slate-700">Logout</Button>
-        <Button></Button>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-blue-600 font-bold mb-2 text-3xl ">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">Welcome, please enter your details</p>
+        <form action="">
+          <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"/>
+          <InputForm label="Password" type="password" placeholder="******" name="password"/>
+
+          <Button classname="bg-blue-600 w-full">Login</Button>
+        </form>
       </div>
     </div>
   )
--- e-commerce/src/components/Elements/Button/index.jsx
+++ e-commerce/src/components/Elements/Button/index.jsx
@@ -1,7 +1,7 @@
     const Button = (props) => {
-    const {children, variant = "bg-black" } = props;
+    const {children, classname = "bg-black" } = props;
     return (
-        <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} 
+        <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} 
         type="submit"
         >
           {children}
