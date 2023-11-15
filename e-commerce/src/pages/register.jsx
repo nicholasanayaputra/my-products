21cb2180198d1f34de4c8fa63d79eea166577d67
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2023-11-15 15:14:07 +0700

    mengimplementasikan Confitional Rendering

--- e-commerce/src/pages/register.jsx
+++ e-commerce/src/pages/register.jsx
@@ -1,15 +1,12 @@
 import AuthLayout from "../components/Layouts/AuthLayout"
 import FormRegister from "../components/Fragments/FormRegister"
-import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
   return (
     <>
-    <AuthLayout title="register">
+    <AuthLayout title="register" type="register">
     <FormRegister />
-    <p className="text-sm mt-5 text-center">
-      Have an account? <Link to="/login" className="font-bold text-blue-600">Login</Link>
-    </p>
+   
     </AuthLayout>
     </>
   )
