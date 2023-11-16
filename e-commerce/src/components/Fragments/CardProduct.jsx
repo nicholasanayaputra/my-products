3ca9d6be750e46225c91a1e82cf97cfdd851e8f1
nicholasanayaputra <146567253+nicholasanayaputra@users.noreply.github.com>
2023-11-16 14:31:22 +0700

    looping menggunakan map agar code lebih pendek di bagian products

--- e-commerce/src/components/Fragments/CardProduct.jsx
+++ e-commerce/src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between ">
       {children}
     </div>
   );
@@ -19,12 +19,12 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-  const { children, title } = props;
+  const { children, name } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+          {name}
         </h5>
         <p className="text-sm text-white">{children}</p>
       </a>
--- e-commerce/src/pages/Products.jsx
+++ e-commerce/src/pages/Products.jsx
@@ -1,28 +1,38 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "New Shoes",
+    price: "Rp 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias.
+    Obcaecati, numquam excepturi, modi temporibus omnis porro dolorum
+    cumque laudantium veritatis nulla non maxime. Obcaecati tempore saepe
+    commodi mollitia pariatur!`,
+  },
+  {
+    id: 2,
+    name: "Old Shoes",
+    price: "Rp 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias.
+    `,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="New Shoes">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias.
-          Obcaecati, numquam excepturi, modi temporibus omnis porro dolorum
-          cumque laudantium veritatis nulla non maxime. Obcaecati tempore saepe
-          commodi mollitia pariatur!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="New Shoes">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, alias.
-          Obcaecati, numquam excepturi, modi temporibus omnis porro dolorum
-          cumque laudantium veritatis nulla non maxime. Obcaecati tempore saepe
-          commodi mollitia pariatur!
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
